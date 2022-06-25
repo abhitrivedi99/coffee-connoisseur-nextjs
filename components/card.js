85215230167dd28b3947c144a575878a7621e8a6
abhitrivedi99 <abhitrivedi99@gmail.com>
2022-06-25 21:54:58 +0530

    feat(card) coffee card component added

---
+++ components/card.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import classNames from 'classnames';
+
+import styles from './card.module.css';
+
+const Card = ({ href, name, imgUrl }) => {
+  return (
+    <Link href={href}>
+      <a className={styles.cardLink}>
+        <div className={classNames('glass', styles.container)}>
+          <div className={styles.cardHeaderWrapper}>
+            <h2 className={styles.cardHeader}>{name}</h2>
+          </div>
+          <div className={styles.cardImageWrapper}>
+            <Image className={styles.cardImage} src={imgUrl} width={260} height={160} alt={name} />
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default Card;
