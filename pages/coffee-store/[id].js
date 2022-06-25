3fb87d09e7ace8a8b0ebb9ef29bafcab50fb24a5
abhitrivedi99 <abhitrivedi99@gmail.com>
2022-06-25 23:22:43 +0530

    feat(store-page) store page & css added
    home page css udpated

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,17 +1,77 @@
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+import classNames from 'classnames';
 
 import coffeeStores from '../../data/coffee-stores.json';
+import styles from '../../styles/coffee-store.module.css';
 
-const CoffeeStore = (props) => {
-  const router = useRouter();
-
-  const { query } = router;
-
-  if (router.isFallback) {
+const CoffeeStore = ({ coffeeStore }) => {
+  const { query, isFallback } = useRouter();
+  if (isFallback) {
     return <div>Loading..</div>;
-  } else {
-    return <div>You are on page with {query.id}</div>;
   }
+
+  const { address, name, neighbourhood, websiteUrl, imgUrl } = coffeeStore;
+
+  const handleUpvoteBtn = () => {
+    console.log('Clicked');
+  };
+
+  return (
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+        <meta name="description" content={`${name} coffee store`} />
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={classNames('glass', styles.col2)}>
+          {address && (
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/places.svg" width="24" height="24" alt="places icon" />
+              <p className={styles.text}>{address}</p>
+            </div>
+          )}
+          {neighbourhood && (
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/nearMe.svg" width="24" height="24" alt="near me icon" />
+              <p className={styles.text}>{neighbourhood}</p>
+            </div>
+          )}
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width="24" height="24" alt="star icon" />
+            <p className={styles.text}>{1}</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvoteBtn}>
+            Up vote!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoffeeStore;
@@ -30,9 +90,7 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps(staticProps) {
-  const params = staticProps.params;
-
+export async function getStaticProps({ params }) {
   const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
     return coffeeStore.id.toString() === params.id; //dynamic id
   });
--- pages/index.js
+++ pages/index.js
@@ -18,26 +18,30 @@ export default function Home({ storeList }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="allows you to discover coffee stores" />
       </Head>
+
       <main className={styles.main}>
         <Banner buttonText={'View stores nearby'} handleOnClick={handleOnBannerBtnClick} />
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} alt="hero image" />
         </div>
         {storeList.length && (
-          <>
-            <h2 className={styles.heading2}>Ahmedabad Stores</h2>
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Stores near Me</h2>
             <div className={styles.cardLayout}>
               {storeList.map((store) => (
                 <Card
                   key={store.id}
                   name={store.name}
                   className={styles.card}
-                  imgUrl={store.imgUrl}
+                  imgUrl={
+                    store.imgUrl ||
+                    'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                  }
                   href={`/coffee-store/${store.id}`}
                 />
               ))}
             </div>
-          </>
+          </div>
         )}
       </main>
     </div>
